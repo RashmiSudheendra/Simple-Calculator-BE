6cb45afce415adc980c6d253d2d67f9cfa64f46a
Rashmi Sudheendra <Rashmi.s11219@gmail.com>
2023-07-09 20:19:40 +0530

    Fourth commit

--- Controllers/CalculationController.cs
+++ Controllers/CalculationController.cs
@@ -2,8 +2,6 @@ using Backend.Models;
 using Backend.Services;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace Backend.Controllers
 {
     [Route("api/[controller]")]
